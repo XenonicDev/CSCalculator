4d98a1c8426f72b3f71824eb6472ec4230219b09
Andrew Depke <andrewdepke@gmail.com>
2018-02-04 19:08:24 -0700

    Added ExpressionBuilder Class, Setup References

---
+++ CSCalculator/Core/ExpressionBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCalculator.Core
+{
+    public class ExpressionBuilder
+    {
+        private static StringBuilder ExpBuilder;
+
+        public static void Add(string Value)
+        {
+            ExpBuilder.Append(Value);
+        }
+
+        public static void Remove(int Index)
+        {
+            ExpBuilder.Remove(Index, 1);
+        }
+
+        public static void Clear()
+        {
+            ExpBuilder.Clear();
+        }
+
+        public static string GetExpression()
+        {
+            return ExpBuilder.ToString();
+        }
+    }
+}
